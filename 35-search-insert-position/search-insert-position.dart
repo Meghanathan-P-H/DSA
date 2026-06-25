2d704217d99d083f7484d84134946166fa0e84ad
MEGHANATHAN P H <161948307+Meghanathan-P-H@users.noreply.github.com>
2026-06-25 23:32:45 +0530

    Time: 0 ms (100.00%) | Memory: 151 MB (35.45%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.dart
@@ -0,0 +1,18 @@
+class Solution {
+  int searchInsert(List<int> nums, int target) {
+     int left = 0, right = nums.length - 1;
+    while(left <= right) {
+        final mid = left + (right - left) ~/ 2;
+        if(nums[mid] == target) {
+            return mid;
+        }
+        else if(target < nums[mid]) {
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+    return left;
+  }
+}
